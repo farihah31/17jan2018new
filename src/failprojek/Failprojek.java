f0da5f0afeacbb866fc26c21f5a77b225c160e3a
anis <anis@farahanis>
2018-01-17 10:30:53 +0800

    commit first time alter anis

---
+++ src/failprojek/Failprojek.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package failprojek;
+
+/**
+ *
+ * @author anis
+ */
+public class Failprojek {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     System.out.println("welcome");
+    System.out.println("saya anis");
+    System.out.println("test coding");
+    }
+    
+}
